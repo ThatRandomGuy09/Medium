fc37a0abcb52319216615e364ad71b23636d2dc8
Vansh Vasishtha <vansh19092002@gmail.com>
2024-08-26 21:41:01 +0530

    Updated index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,37 +1,15 @@
 import { Hono } from "hono";
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { userRouter } from "./routes/user";
+// import { bookRouter } from "./routes/blog";
 
-const app = new Hono<{
+export const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
 }>();
 
-app.post("/api/v1/signup", (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate());
-
-  return c.text("signup route");
-});
-
-app.post("/api/v1/signin", (c) => {
-  return c.text("signin route");
-});
-
-app.get("/api/v1/blog/:id", (c) => {
-  const id = c.req.param("id");
-  console.log(id);
-  return c.text("get blog route");
-});
-
-app.post("/api/v1/blog", (c) => {
-  return c.text("signin route");
-});
-
-app.put("/api/v1/blog", (c) => {
-  return c.text("signin route");
-});
+app.route("/api/v1/user", userRouter);
+// app.route("/api/v1/book", bookRouter);
 
 export default app;
