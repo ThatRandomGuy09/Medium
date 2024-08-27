0a19b44e452f3c14ca5bb912dabcdab0b191ba19
Vansh Vasishtha <vansh19092002@gmail.com>
2024-08-27 19:58:45 +0530

    Added zod through personal NPM

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -2,6 +2,10 @@ import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 import { verify } from "hono/jwt";
+import {
+  createPostInput,
+  updatePostInput,
+} from "@thatrandomguy019/medium-common";
 
 export const blogRouter = new Hono<{
   Bindings: {
@@ -31,8 +35,13 @@ blogRouter.use("/*", async (c, next) => {
 });
 
 blogRouter.post("/", async (c) => {
-  const userId = c.get("userId");
   const body = await c.req.json();
+  const { success } = createPostInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({ error: "invalid input" });
+  }
+  const userId = c.get("userId");
   const prisma = new PrismaClient({
     datasourceUrl: c.env?.DATABASE_URL,
   }).$extends(withAccelerate());
@@ -47,6 +56,34 @@ blogRouter.post("/", async (c) => {
   return c.json({
     id: post.id,
   });
+});
+
+blogRouter.put("/", async (c) => {
+  const body = await c.req.json();
+  const { success } = updatePostInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({ error: "invalid input" });
+  }
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const post = await prisma.post.update({
+      where: {
+        id: body.id,
+      },
+      data: {
+        title: body.title,
+        content: body.content,
+      },
+    });
+    return c.json({
+      post,
+    });
+  } catch (e: any) {
+    console.log(e);
+  }
 });
 
 // Add Pagination after the Blog fetching.
@@ -76,28 +113,5 @@ blogRouter.get("/:id", async (c) => {
     });
   } catch (e) {
     c.status(411);
-  }
-});
-
-blogRouter.put("/", async (c) => {
-  const body = await c.req.json();
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env?.DATABASE_URL,
-  }).$extends(withAccelerate());
-  try {
-    const post = await prisma.post.update({
-      where: {
-        id: body.id,
-      },
-      data: {
-        title: body.title,
-        content: body.content,
-      },
-    });
-    return c.json({
-      post,
-    });
-  } catch (e: any) {
-    console.log(e);
   }
 });
