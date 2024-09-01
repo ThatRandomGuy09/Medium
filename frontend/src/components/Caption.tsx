c419cf45b609be6f518e52480200c15a486f2d59
Vansh Vasishtha <vansh19092002@gmail.com>
2024-09-01 18:33:04 +0530

    Caption Created

---
+++ frontend/src/components/Caption.tsx
@@ -0,0 +1,9 @@
+export const Caption = () => {
+  return (
+    <div className="bg-gray-200 h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div className="max-w-md text-4xl font-semibold">hey</div>
+      </div>
+    </div>
+  );
+};
