e3854fb8c08f6ec8e1a499906a43122819f11c9a
Vansh Vasishtha <vansh19092002@gmail.com>
2024-09-01 18:33:52 +0530

    Signin Created

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,1 +1,10 @@
-export const Signup = () => {};
+import { Caption } from "../components/Caption";
+
+export const Signup = () => {
+  return (
+    <div className="grid grid-cols-2">
+      <div></div>
+      <Caption />
+    </div>
+  );
+};
