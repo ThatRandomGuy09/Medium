490bcd631f6ca19d18c01d07f6980d1b73b2afe0
Vansh Vasishtha <vansh19092002@gmail.com>
2024-09-01 18:33:34 +0530

    Updated App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+import { Blog } from "./pages/Blog";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
         <Route path="/blog/:id" element={<Blog />} />
       </Routes>
     </BrowserRouter>
