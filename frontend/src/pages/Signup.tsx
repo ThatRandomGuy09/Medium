a99c1b2e0dc1211f0be66041e1a1ffece8b4ca64
Vansh Vasishtha <vansh19092002@gmail.com>
2024-09-03 18:10:18 +0530

    Signup Updated

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,10 +1,16 @@
 import { Caption } from "../components/Caption";
+import { Auth } from "../components/Auth";
 
 export const Signup = () => {
   return (
     <div className="grid grid-cols-2">
-      <div></div>
-      <Caption />
+      <div>
+        <Auth />
+      </div>
+
+      <div className="invisible lg:visible">
+        <Caption />
+      </div>
     </div>
   );
 };
