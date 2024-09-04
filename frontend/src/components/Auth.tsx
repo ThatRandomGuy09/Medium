68990e0b4f61059832bb895eb303536af910b74e
Vansh Vasishtha <vansh19092002@gmail.com>
2024-09-04 18:50:50 +0530

    Updated Auth Components

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,7 +1,26 @@
-export const Auth = () => {
-    return(
-        <>
-        Hello
-        </>
-    )
+import { Link } from "react-router-dom";
+
+export const Auth = ({ type }: { type: "signup" | "signin" }) => {
+  return (
+    <div className="h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div>
+          <div className="px-10">
+            <div className="text-3xl font-extrabold">Create an account</div>
+            <div className="text-slate-500">
+              {type === "signin"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+              <Link
+                className="pl-2 underline"
+                to={type === "signin" ? "/signup" : "/signin"}
+              >
+                {type === "signin" ? "Sign up" : "Sign in"}
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
